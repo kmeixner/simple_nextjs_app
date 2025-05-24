8f3709e4f8a95925d4c6f27bff7bb6fce2671528
Kevin Meixner <kmeixner42@gmail.com>
2025-05-24 12:50:18 -0400

    likeButton=>now client component in own module
    new file:   app/like-button.js
    modified:   app/page.js

--- app/page.js
+++ app/page.js
@@ -1,31 +1,21 @@
-import { useState } from 'react';
+import LikeButton from './like-button';
 
 function Header({ title }) {
-    return <h1>{title ? title : 'Default title'}</h1>;
+  return <h1>{title ? title : 'Default title'}</h1>;
 }
 
 export default function HomePage() {
-    /* Note: 'export default' above tells Next.js distinguish
-       what is the main component to render for your page  */
+  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
 
-    const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
-
-    const [likes, setLikes] = useState(0);
-
-    function handleClick() {
-        setLikes(likes + 1);
-    }
-
-    return (
+  return (
     <div>
-        <Header title="Develop. Preview. Ship." />
-        <ul>
+      <Header title="Develop. Preview. Ship." />
+      <ul>
         {names.map((name) => (
-            <li key={name}>{name}</li>
+          <li key={name}>{name}</li>
         ))}
-        </ul>
-
-        <button onClick={handleClick}>Like ({likes})</button>
+      </ul>
+      <LikeButton />
     </div>
-    );
+  );
 }
